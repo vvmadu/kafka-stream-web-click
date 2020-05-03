1eb52bb4326f6d6a7545500f16bdf86b989a0d12
Venkata Madhu <vvmadhu@yahoo.com>
2020-05-03 07:38:25 +0530

    Create ApplicationConstants.java

---
+++ src/main/java/com/hcl/dna/ads/framework/process/util/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package com.hcl.dna.ads.framework.process.util;
+
+public class ApplicationConstants {
+    public static final String PRODUCT_TOPIC = "PRODUCT";
+    public static final String CART_TOPIC = "ORDER";
+    public static final String CUSTOMER_TOPIC = "CUSTOMER";
+	public static final String SUBJECTS = "subjects";
+    public static final String FORWARD_SLASH = "/";
+    public static final String VERSIONS = "versions";
+
+    public static final String CURRENT_PRODUCT_TOPIC = "PRODUCT-CURRENT";
+    public static final String TRAGET_TOPIC = "TEST";
+}
